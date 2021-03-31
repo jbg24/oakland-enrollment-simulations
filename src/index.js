abd8741c5bcde67fb2427bb83160ad9918e60e2f
tylermachado <3586466+tylermachado@users.noreply.github.com>
2021-03-30 20:39:12 -0400

    adding shapefiles

--- src/index.js
+++ src/index.js
@@ -13,30 +13,111 @@ class Map extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      lng: -122.26,
-      lat: 37.79,
-      zoom: 12,
+      lng: -122.214,
+      lat: 37.796,
+      zoom: 11,
       vision: "Today",
+      visibleToday: 'visible',
+      visibleZone: 'none'
     };
     this.changeVision = this.changeVision.bind(this);
     this.mapContainer = React.createRef();
   }
   changeVision() {
     if (this.state.vision === "Today") {
-      this.setState({ vision: "Zone" })
+      this.setState({ 
+        vision: "Zone",
+        visibleToday: 'none',
+        visibleZone: 'visible'
+      });
     } else {
-      this.setState({ vision: "Today" })
+      this.setState({ 
+        vision: "Today",
+        visibleToday: 'visible',
+        visibleZone: 'none'
+      })
     }
   }
   componentDidMount() {
-    console.log(data);
-    const { lng, lat, zoom } = this.state;
+    const { lng, lat, zoom, vision, visibleToday, visibleNone } = this.state;
     const map = new mapboxgl.Map({
       container: this.mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom: zoom
     });
+
+    map.on('load', function () {
+      map.addSource('bounds-today', {
+        type: 'vector',
+        url: 'mapbox://tylermachado.ctywuxms'
+      });
+
+    map.addLayer({
+      'id': 'bounds-today-data',
+      'type': 'fill',
+      'source': 'bounds-today',
+      'source-layer': 'OUSD_ESAA_1920-4yapi9',
+      'layout': {
+        'visibility': visibleToday
+      },
+      'paint': {
+        'fill-color': [
+          "rgb",
+          0,
+          ["-", ["get", "AAID"], 101],
+          ["*", 1, ["get", "AAID"]]
+        ],
+        'fill-opacity': 1
+      }
+    });
+
+      map.addSource('bounds-vision', {
+        type: 'vector',
+        url: 'mapbox://tylermachado.3a0n7mkn'
+      });
+
+      map.addLayer({
+        'id': 'bounds-vision-data',
+        'type': 'fill',
+        'source': 'bounds-vision',
+        'source-layer': 'Dissolved_OUSD_ES_Multi_Schoo-a87qn7',
+        'layout': {
+          'visibility': visibleNone
+        },
+        'paint': {
+          'fill-color': [
+            "rgb",
+            255,
+            161,
+            ["*", 63, ["get", "Zone_5"]]
+          ],
+          'fill-opacity': 1
+        }
+      });
+
+      map.addSource('census-blocks', {
+        type: 'vector',
+        url: 'mapbox://tylermachado.3unavpo1'
+      });
+
+      map.addLayer({
+        'id': 'census-blocks-data',
+        'type': 'line',
+        'source': 'census-blocks',
+        'source-layer': 'OUSD_CBG_With_Wealth_Data-2evk4o',
+        'layout': {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        'paint': {
+          'line-color': '#ff69b4',
+          'line-width': 1
+        }
+      });
+    })
+
+    
 
     map.on('move', () => {
       this.setState({
@@ -46,6 +127,7 @@ class Map extends React.PureComponent {
       });
     });
   }
+
   render() {
     const { lng, lat, zoom } = this.state;
     return (
